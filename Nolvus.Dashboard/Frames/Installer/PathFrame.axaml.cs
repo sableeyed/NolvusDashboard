3147928e5439475faca8c05ea58306e0da63e570
sableeyed <1906931+sableeyed@users.noreply.github.com>
2025-11-15 19:55:44 -0600

    Redo frame layout

---
+++ Nolvus.Dashboard/Frames/Installer/PathFrame.axaml.cs
@@ -0,0 +1,13 @@
+// using Nolvus.Core.Frames;
+// using Nolvus.Core.Interfaces;
+
+// namespace Nolvus.Dashboard.Frames.Installer
+// {
+//     public partial class PathFrame : DashboardFrame
+//     {
+//         public PathFrame(IDashboard Dashboard, FrameParameters Params) :base(Dashboard, Params)
+//         {
+//             // InitializeComponents();
+//         }
+//     }  
+// }

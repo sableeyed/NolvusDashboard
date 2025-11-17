93a7d943b420285f97c2e896435a3fbaf72c5abc
Madeline <1906931+sableeyed@users.noreply.github.com>
2025-11-17 16:09:08 -0600

    CDN Frame + wiring

---
+++ Nolvus.Dashboard/Frames/Installer/CDNFrame.axaml.cs
@@ -0,0 +1,64 @@
+using Nolvus.Core.Frames;
+using Nolvus.Core.Interfaces;
+using Nolvus.Core.Services;
+using Nolvus.Core.Misc;
+using Nolvus.Core.Enums;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+
+namespace Nolvus.Dashboard.Frames.Installer
+{
+    public partial class CDNFrame : DashboardFrame
+    {
+        public CDNFrame(IDashboard Dashboard, FrameParameters Params) :base(Dashboard, Params)
+        {
+            InitializeComponent();
+
+            BtnPrevious.Click += BtnPrevious_Click;
+            BtnContinue.Click += BtnContinue_Click;
+        }
+
+        protected override async Task OnLoadedAsync()
+        {
+            DrpDwnLstDownLoc.ItemsSource = CDN.Get();
+            DrpDwnLstDownLoc.SelectedIndex = DownloadLocationIndex(CDN.Get());
+            ServiceSingleton.Dashboard.Info("CDN Location");
+
+        }
+
+        private void OnCDNLocationChanged(object? sender, SelectionChangedEventArgs e)
+        {
+            ServiceSingleton.Instances.WorkingInstance.Settings.CDN = DrpDwnLstDownLoc.SelectedItem.ToString();
+        }
+
+        private int DownloadLocationIndex(List<string> Locations)
+        {
+            if (ServiceSingleton.Instances.WorkingInstance.Settings.CDN != string.Empty)
+            {
+                var Index = Locations.FindIndex(x => x == ServiceSingleton.Instances.WorkingInstance.Settings.CDN);
+
+                return Index == -1 ? 0 : Index;
+            }
+
+            return 0;
+        }
+
+        private void BtnPrevious_Click(object? sender, RoutedEventArgs e)
+        {
+            ServiceSingleton.Dashboard.LoadFrame<PageFileFrame>();
+        }
+
+        private void BtnContinue_Click(object? sender, RoutedEventArgs e)
+        {
+            switch (ServiceSingleton.Instances.WorkingInstance.Name)
+            {
+                case Strings.NolvusAscension:
+                    //TODO v5 summary frame
+                    break;
+                case Strings.NolvusAwakening:
+                    //TODO v6 summary frame
+                    break;
+            }
+        }
+    }
+}
--- Nolvus.Dashboard/Frames/Installer/PageFileFrame.axaml.cs
+++ Nolvus.Dashboard/Frames/Installer/PageFileFrame.axaml.cs
@@ -1,7 +1,12 @@
 using Avalonia.Input;
+using Avalonia.Interactivity;
+using Avalonia.Controls;
 using Nolvus.Core.Frames;
 using Nolvus.Core.Interfaces;
 using System.Diagnostics;
+using Nolvus.Core.Services;
+using Nolvus.Core.Misc;
+using Nolvus.Dashboard.Controls;
 
 namespace Nolvus.Dashboard.Frames.Installer
 {
@@ -10,6 +15,52 @@ namespace Nolvus.Dashboard.Frames.Installer
         public PageFileFrame(IDashboard Dashboard, FrameParameters Params) :base(Dashboard, Params)
         {
             InitializeComponent();
+
+            BtnPrevious.Click += BtnPrevious_Click;
+            BtnContinue.Click += BtnContinue_Click;
+        }
+
+        protected override async Task OnLoadedAsync()
+        {
+            ServiceSingleton.Dashboard.Info("Swap size configuration");
+        }
+
+        private void BtnPrevious_Click(object? sender, RoutedEventArgs e)
+        {
+            switch (ServiceSingleton.Instances.WorkingInstance.Name)
+            {
+                case Strings.NolvusAscension:
+                    ServiceSingleton.Dashboard.LoadFrame<v5.ENBFrame>();
+                    break;
+                case Strings.NolvusAwakening:
+                    Console.WriteLine("v6 not implemented");
+                    break;
+            }
+        }
+
+        private async void BtnContinue_Click(object? sender, RoutedEventArgs e)
+        {
+            var owner = TopLevel.GetTopLevel(this) as Window;
+            bool? response = await NolvusMessageBox.ShowConfirmation(owner, "Confirmation", "Swap configuration is REALLY IMPORTANT to avoid crashes, be sure you set it up correctly. Do you want to continue?");
+            if(response == true)
+            {
+                if (NexusApi.ApiManager.AccountInfo.IsPremium)
+                {
+                    ServiceSingleton.Dashboard.LoadFrame<CDNFrame>();
+                }
+                else
+                {
+                    switch (ServiceSingleton.Instances.WorkingInstance.Name)
+                    {
+                        case Strings.NolvusAscension:
+                            //ServiceSingleton.Dashboard.LoadFrame<v5.SummaryFrame>();
+                            break;
+                        case Strings.NolvusAwakening:
+                            //ServiceSingleton.Dashboard.LoadFrame<v6.SummaryFrame<>();
+                            break;
+                    }
+                }
+            }
         }
 
         private void LnkPageFile_PointerPressed(object? sender, PointerPressedEventArgs e)

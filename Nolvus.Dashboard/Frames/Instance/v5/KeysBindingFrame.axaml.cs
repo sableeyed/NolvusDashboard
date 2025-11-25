54602b0b652a414164c5f4557ad2f0d6d1823d25
Madeline <1906931+sableeyed@users.noreply.github.com>
2025-11-25 12:13:51 -0600

    More Frames

--- Nolvus.Dashboard/Frames/Instance/v5/KeysBindingFrame.axaml.cs
+++ Nolvus.Dashboard/Frames/Instance/v5/KeysBindingFrame.axaml.cs
@@ -1,5 +1,7 @@
 using Nolvus.Core.Frames;
 using Nolvus.Core.Interfaces;
+using Nolvus.Core.Services;
+using Avalonia.Interactivity;
 
 namespace Nolvus.Dashboard.Frames.Instance.v5
 {
@@ -8,6 +10,13 @@ namespace Nolvus.Dashboard.Frames.Instance.v5
         public KeysBindingFrame(IDashboard Dashboard, FrameParameters Params) :base(Dashboard, Params)
         {
             InitializeComponent();
+
+            BtnBack.Click += BtnBack_Click;
+        }
+
+        private void BtnBack_Click(object? sender, RoutedEventArgs e)
+        {
+            ServiceSingleton.Dashboard.LoadFrame<InstancesFrame>();
         }
     }
 }
